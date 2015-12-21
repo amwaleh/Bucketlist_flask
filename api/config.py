bb6a40d577c37b3e9e0b4f4c39c5dd4beee4527f
Mwaleh muturi <alex.mwaleh@andela.com>
2015-12-21 13:04:58 +0300

    [fixes] travis Failing Test

--- api/config.py
+++ api/config.py
@@ -1,5 +1,8 @@
+import os
+
 POSTS_PER_PAGE = 20
 MAX_PAGES = 100
+if 'travis' is not os.environ
 TEST_DB = 'postgresql://127.0.0.1/Bucket_test'
 DATABASE_URI = 'postgresql://localhost/flask'
 SECRET = "development-Key2"
